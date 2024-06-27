2313ec7c4d39fe9bd7705f2b99bca9845118850d
Nguyen Tien Phat <phatlamson9468@gmail.com>
2024-06-27 17:03:16 +0700

    Change port to 5000

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,10 @@ import app from './app';
 import connectDB from './config/Mongo.database';
 require('dotenv').config();
 
-
-
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on 5000`);
 });
